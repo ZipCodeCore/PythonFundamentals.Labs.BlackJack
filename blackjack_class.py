c0994dc3d2b4c53034a2f2781d71264546cc8fc4
Preeti Sehgal <psehgal@zipcodes-mbp-4.lan>
2020-03-28 00:04:17 -0400

    created 2 .py files and defined the structure of the code

---
+++ blackjack_class.py
@@ -0,0 +1,23 @@
+#Create classes for blackjack program
+#This .py file will work as a module of classes
+#That will be imported in the main program
+#All classes will be part of this module and game willl be a separate .py program file
+
+
+
+
+### A function for creating a deck
+# The deck will contain cards of Suits: Heart, Spade, Diamond, Clubs  and Ranks: Ace, 2-10, Jack, Queen, King
+# The function will return a shuffled deck with 52 cards
+
+
+
+# A function for creating player and dealers hands
+#The player and dealer will get 2 cards initially
+
+
+
+# A function for counting the points
+# This function will count the points of the player and the dealer
+# The point calculation logic will also determine the value of ACE as 1 or 11 depending on the situation
+
---
+++ blackjack_gameloop.py
@@ -0,0 +1,16 @@
+#This program will import the classes module
+#And use the classes and finction from the import module to runt he game
+
+
+
+#Call the function to craete a shuffled deck of 52 cards
+
+
+#Deal the cards to the player and the dealer
+
+
+#Ask the player to Hist opr Stand
+
+
+
+#Calculate the points and decide who busts, player or the dealer
