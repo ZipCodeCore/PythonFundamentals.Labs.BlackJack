3298e4d4cb2d36cbc0925312b430d324e6f6359f
Preeti Sehgal <psehgal@zipcodes-mbp-4.lan>
2020-03-28 01:37:40 -0400

    created class and function for creating and shuffeling the cards and also wrote a unit test for the same and it ran successfully

--- blackjack_class.py
+++ blackjack_class.py
@@ -1,14 +1,33 @@
+
+from random import shuffle
+
 #Create classes for blackjack program
 #This .py file will work as a module of classes
 #That will be imported in the main program
 #All classes will be part of this module and game willl be a separate .py program file
 
+class Deck:
+    def __init__(self):
+        self = self
 
-
-
-### A function for creating a deck
+# A function for creating a deck
 # The deck will contain cards of Suits: Heart, Spade, Diamond, Clubs  and Ranks: Ace, 2-10, Jack, Queen, King
+    def deck_cards(self):
+        result = []
+        suit = ['H', 'S', 'D', 'C']
+        num = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+        for i in suit:
+            for n in num:
+                result.append(i+n)
+
 # The function will return a shuffled deck with 52 cards
+        shuffle(result)
+
+        return result
+
+
+
+
 
 
 
@@ -20,4 +39,8 @@
 # A function for counting the points
 # This function will count the points of the player and the dealer
 # The point calculation logic will also determine the value of ACE as 1 or 11 depending on the situation
+
+if __name__ == "__main__":
+    mydeck = Deck()
+    print(mydeck.deck_cards())
 
---
+++ test_class.py
@@ -0,0 +1,13 @@
+import unittest
+import blackjack_class
+
+class TestDeck(unittest.TestCase):
+    def test_deckcards(self):
+        expected_list = [
+            'HA', 'H2', 'H3', 'H4', 'H5', 'H6', 'H7', 'H8', 'H9', 'H10', 'HJ', 'HQ', 'Hk',
+            'SA', 'S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8', 'S9', 'S10', 'SJ', 'SQ', 'Sk',
+            'DA', 'D2', 'D3', 'D4', 'D5', 'D6', 'D7', 'D8', 'D9', 'D10', 'DJ', 'DQ', 'Dk',
+            'CA', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9', 'C10', 'CJ', 'CQ', 'Ck',
+            ]
+        testing_class = blackjack_class.Deck()
+        self.assertEqual(testing_class.deck_cards().sort(), expected_list.sort())
